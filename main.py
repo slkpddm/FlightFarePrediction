0d89fc003c8da03968cdc8b8966f309bdc33cb60
slkpddm <slkpddm@gmail.com>
2023-12-11 16:30:46 +0530

    Data validation component completed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from FlightFarePredictor.components.data_validation import DataValidation
 from FlightFarePredictor.logger import logging
 from FlightFarePredictor.exception import SensorException
 from FlightFarePredictor.utils import get_collection_as_dataframe
@@ -13,5 +14,12 @@ try:
      print(data_ingestion_config.to_dict())
      data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
      data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+     #data validation
+     data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+     data_validation = DataValidation(data_validation_config=data_validation_config,
+                        data_ingestion_artifact=data_ingestion_artifact)
+
+     data_validation_artifact = data_validation.initiate_data_validation()
 except Exception as e:
      print(e)
